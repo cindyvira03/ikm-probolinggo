15563312c48f75ca7fdebe0e3105222b9f6d264d
cindy vira safitri <cindyvira929@email.com>
2026-04-10 19:37:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,42 +12,41 @@ use App\Http\Middleware\isUser;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('rumah-batik', [HomeController::class, 'rumahBatik'])->name('rumah-batik');
-Route::get('produk-ikm', [HomeController::class, 'produkIkm'])->name('produk-ikm');
-Route::get('produk-ikm/kategori/{slug}', [HomeController::class, 'produkByKategori'])->name('produk-ikm.kategori');
-Route::get('produk-ikm/{id}', [HomeController::class, 'produkIkmDetail'])->name('produk-ikm.detail');
-Route::get('outlet-ikm', [HomeController::class, 'outletIkm'])->name('outlet-ikm');
-Route::get('profil-ikm', [HomeController::class, 'profilIkm'])->name('profil-ikm');
-Route::get('profil-ikm/{id}', [HomeController::class, 'profilIkmDetail'])->name('profil-ikm.detail');
+// Route::get('/', [HomeController::class, 'index'])->name('home');
+// Route::get('rumah-batik', [HomeController::class, 'rumahBatik'])->name('rumah-batik');
+// Route::get('produk-ikm', [HomeController::class, 'produkIkm'])->name('produk-ikm');
+// Route::get('produk-ikm/kategori/{slug}', [HomeController::class, 'produkByKategori'])->name('produk-ikm.kategori');
+// Route::get('produk-ikm/{id}', [HomeController::class, 'produkIkmDetail'])->name('produk-ikm.detail');
+// Route::get('outlet-ikm', [HomeController::class, 'outletIkm'])->name('outlet-ikm');
+// Route::get('profil-ikm', [HomeController::class, 'profilIkm'])->name('profil-ikm');
+// Route::get('profil-ikm/{id}', [HomeController::class, 'profilIkmDetail'])->name('profil-ikm.detail');
 
-// Auth::routes();
+//  Auth::routes();
 
-Route::prefix('user-area')->middleware(['auth', isUser::class])->name('user.')->group(function () {
-    Route::get('/', function () {
-        return redirect()->route('user.home');
-    });
-    Route::get('dashboard', [UserDashboardController::class, 'index'])->name('home');
-    Route::resource('outlet', OutletController::class);
-    Route::resource('produk', ProdukController::class);
+// Route::prefix('user-area')->middleware(['auth', isUser::class])->name('user.')->group(function () {
+//     Route::get('/', function () {
+//         return redirect()->route('user.home');
+//     });
+//     Route::get('dashboard', [UserDashboardController::class, 'index'])->name('home');
+//     Route::resource('outlet', OutletController::class);
+//     Route::resource('produk', ProdukController::class);
 
-    Route::get('profile', [UserController::class, 'editProfile'])->name('profile.edit');
-    Route::put('profile', [UserController::class, 'updateProfile'])->name('profile.update');
-});
+//     Route::get('profile', [UserController::class, 'editProfile'])->name('profile.edit');
+//     Route::put('profile', [UserController::class, 'updateProfile'])->name('profile.update');
+// });
 
-Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+// Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () {
+//     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-    // Kategori routes
-    Route::resource('kategori', KategoriController::class);
+   
+//     Route::resource('kategori', KategoriController::class);
 
-    // Profil IKM routes (read-only: index, show, edit, update)
-    Route::get('profil-ikm', [ProfilIkmController::class, 'index'])->name('profil-ikm.index');
-    Route::get('profil-ikm/{id}', [ProfilIkmController::class, 'show'])->name('profil-ikm.show');
-    Route::get('profil-ikm/{id}/edit', [ProfilIkmController::class, 'edit'])->name('profil-ikm.edit');
-    Route::put('profil-ikm/{id}', [ProfilIkmController::class, 'update'])->name('profil-ikm.update');
-});
+    
+//     Route::get('profil-ikm', [ProfilIkmController::class, 'index'])->name('profil-ikm.index');
+//     Route::get('profil-ikm/{id}', [ProfilIkmController::class, 'show'])->name('profil-ikm.show');
+//     Route::get('profil-ikm/{id}/edit', [ProfilIkmController::class, 'edit'])->name('profil-ikm.edit');
+//     Route::put('profil-ikm/{id}', [ProfilIkmController::class, 'update'])->name('profil-ikm.update');
+// });
+//  Auth::routes();
 
-// Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
